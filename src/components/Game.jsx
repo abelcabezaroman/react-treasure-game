b4fa254fadbf4f15e78901b31c8a85641688ab26
acabezar <contacto@abelcabezaroman.com>
2023-10-19 09:51:28 +0200

    update

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,109 +1,141 @@
 import { useState } from "react";
 
-const defaultData = [
-    [
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-    ], [
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-    ], [
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-    ], [
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-    ], [
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-        {
-            img: "x.png"
-        },
-    ]
+// const defaultData = [
+//   [
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//   ],
+//   [
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//   ],
+//   [
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//   ],
+//   [
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//   ],
+//   [
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//     {
+//       img: "x.png",
+//     },
+//   ],
+// ];
 
-]
+const generateInitialArray = (num, num2) => {
+  const newBiXArray = [];
+  for (let index = 0; index < num; index++) {
+    const newBiYArray = [];
+    for (let index = 0; index < num2; index++) {
+      newBiYArray.push({
+        img: "x.png",
+      });
+    }
+    newBiXArray.push(newBiYArray)
+  }
 
-const random = Math.floor(Math.random() * 5);
-const random2 = Math.floor(Math.random() * 5);
+  return newBiXArray;
+};
+
+const numRow = 5;
+const numCols = 5;
+
+const random = Math.floor(Math.random() * numRow);
+const random2 = Math.floor(Math.random() * numCols);
 
 export default function Game() {
-    const [data, setData] = useState(defaultData);
+  const [data, setData] = useState(generateInitialArray(numRow, numCols));
 
-    const checkGame = (index, indexChild) => {
-        const copyData = [...data];
+  const checkGame = (index, indexChild) => {
+    const copyData = [...data];
 
-        if (random === index && random2 === indexChild) {
-            copyData[index][indexChild].img = "chest.png";
-        } else {
-            copyData[index][indexChild].img = "skull.png";
-        }
-
-        setData(copyData)
+    if (random === index && random2 === indexChild) {
+      copyData[index][indexChild].img = "chest.png";
+    } else {
+      copyData[index][indexChild].img = "skull.png";
     }
 
-    return <div>
-        {data.map((item, index) => item.map((itemChild, indexChild) => <img onClick={() => checkGame(index, indexChild)} key={index + "-" + indexChild}  src={itemChild.img} alt="Imagen de juego" />))}
+    setData(copyData);
+  };
+
+  return (
+    <div>
+      {data.map((item, index) =>
+        item.map((itemChild, indexChild) => (
+          <img
+            onClick={() => checkGame(index, indexChild)}
+            key={index + "-" + indexChild}
+            src={itemChild.img}
+            alt="Imagen de juego"
+          />
+        ))
+      )}
     </div>
+  );
 }
